8a1a8ecb87960f60c6019da74604a0565d015e22
JakeZalesny <jrzalesny@gmail.com>
2025-05-18 04:42:05 -0600

    added Button component (not useable yet)

--- web/fleet-dispatch-web/src/app/page.tsx
+++ web/fleet-dispatch-web/src/app/page.tsx
@@ -4,7 +4,7 @@ import Logo from "./components/Logo";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center justify-center sm-flex:row m:items-start">
         <Logo 
           path="/fleet-dispatch-logo-resized-invert.png"
           alt="Inverted Logo"
@@ -13,7 +13,7 @@ export default function Home() {
           reroute="/"
         />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="flex gap-4 items-center justify-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="/driver"
@@ -21,8 +21,8 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
+              className="light:invert hover:dark:inverted-colors"
+              src="/fleet-dispatch-logo-resized.png"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -35,7 +35,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dispatcher Login
+            Dispatch Login
           </a>
         </div>
       </main>
