61440884e6d6a65972f7d44c2797c02d7a97634f
JakeZalesny <jrzalesny@gmail.com>
2025-05-31 15:31:49 -0600

    started layout for driver home page

--- web/fleet-dispatch-web/src/app/components/Button.tsx
+++ web/fleet-dispatch-web/src/app/components/Button.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 interface ButtonProps {
     children: React.ReactNode;
-    type: "add" | "general" | "invert",
+    type: "add" | "general" | "text",
     className?: string;
     onClick?: () => void;
 }
@@ -31,7 +31,16 @@ export function Button({ children, type, className, onClick }: ButtonProps) {
         </button>
     );  
     }
-    // else if 
+    else if (type === "text") {
+      return (
+        <button
+            className="text-foreground hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+    } 
 
 
     
--- web/fleet-dispatch-web/src/app/driver/dummy/page.tsx
+++ web/fleet-dispatch-web/src/app/driver/dummy/page.tsx
@@ -8,7 +8,22 @@ export default function DummyDriver() {
       })
     return (
         <div>
-            <Calendar type="range"/>
+            <header className="flex flex-col mx-auto mt-15 justify-center space-x-0">
+                <img 
+                src="/jake-in-paris.jpg" 
+                alt="driver image" 
+                width={200} height={200}
+                className="rounded-full mx-auto">
+                </img>
+                <h1 className="text-4xl font-bold text-center mt-6">Jake Zalesny</h1>
+                <h2 className="text-1xl font-bold text-center mt-2">San Diego, CA</h2>
+            </header>
+            <main className="flex flex-row mt-15 space-x-0">
+                <div className="flex flex-col ml-10 justify-center">
+                    <h1 className="text-2xl font-bold text-left mt-6">Requested Time Off</h1>
+                    <Calendar type="range"></Calendar>
+                </div>
+            </main>
         </div>
 
     );
