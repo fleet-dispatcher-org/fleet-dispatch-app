1e8e0b38eeb7eb724308d305c6ef079f8c110d61
JakeZalesny <jrzalesny@gmail.com>
2025-06-02 21:27:47 -0600

    made a report submitted page

---
+++ web/fleet-dispatch-web/src/app/report/report-submitted/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+import Logo from "../../components/Logo";
+
+export default function Done() {
+    return (<div className="flex flex-col mx-auto w-[400px] mt-10 justify-center space-x-0 font-bold text-center">
+            <Logo 
+                                  path="/fleet-dispatch-logo-no-background.png"
+                                  alt="Inverted Logo"
+                                  width={70}
+                                  height={70}
+                                  reroute="/"
+                                  className="mx-auto"
+                                />
+            <h1 className="font-bold text-center mt-4">Thank you for submitting your bug report, a member of our IT team will reach out shortly</h1>
+            <a className="mt-4 hover:underline" href="/">Back to Home</a>
+        </div>);
+}
