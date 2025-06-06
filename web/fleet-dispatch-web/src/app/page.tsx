8ff135583edc0ecba7bcb6832e5f978a3c3ff091
JakeZalesny <jrzalesny@gmail.com>
2025-06-06 17:46:42 -0600

    added authentication

--- web/fleet-dispatch-web/src/app/page.tsx
+++ web/fleet-dispatch-web/src/app/page.tsx
@@ -26,9 +26,9 @@ export default function Home() {
         <div className="flex gap-4 items-center justify-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="/driver"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="/auth/login"
+            // target="_blank"
+            // rel="noopener noreferrer"
           >
             <Image
               className="light:invert hover:dark:inverted-colors"
