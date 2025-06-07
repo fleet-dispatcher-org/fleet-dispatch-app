fabe646d9277ae160073b3afc44a0c8489c52030
JakeZalesny <jrzalesny@gmail.com>
2025-06-06 23:36:14 -0600

    deleted auth0 stuff

--- web/fleet-dispatch-web/src/app/page.tsx
+++ web/fleet-dispatch-web/src/app/page.tsx
@@ -5,6 +5,7 @@ import Logo from "./components/Logo";
 import { Helmet } from "react-helmet";
 import { useEffect } from "react";
 import Button  from "./components/Button";
+import Link from "next/link";
 
 export default function Home() {
   useEffect(() => {
@@ -24,7 +25,7 @@ export default function Home() {
         <h1 className="text-4xl sm:text-6xl font-italic font-bold text-center">Fleet Dispatch</h1>
 
         <div className="flex gap-4 items-center justify-center flex-col sm:flex-row">
-          <a
+          <Link
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="/auth/login"
             // target="_blank"
@@ -38,7 +39,7 @@ export default function Home() {
               height={20}
             />
             Login
-          </a>
+          </Link>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="/register"
