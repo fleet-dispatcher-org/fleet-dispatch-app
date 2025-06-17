8da63724fed37e0f51a9cd9f5e9f9b607013413a
JakeZalesny <jrzalesny@gmail.com>
2025-06-16 19:06:12 -0600

    fixing auth and adapters

--- web/fleet-dispatch-web/auth.ts
+++ web/fleet-dispatch-web/auth.ts
@@ -2,8 +2,8 @@ import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "../fleet-dispatch-web/prisma/prisma";
- 
+import prisma from "./prisma/prisma";
+
 const adapter = PrismaAdapter(prisma);
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
@@ -11,13 +11,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  }), Credentials({
-    credentials: {
-      email: { label: "Email", type: "email" },
-      password: { label: "Password", type: "password" },
-    },
-    async authorize(credentials) {
-      
-    }
-  })],
+  }), 
+],
 })
