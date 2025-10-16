fa7bfa4d75dc82df2a829753caf5e23ee150fc98
JakeZalesny <jrzalesny@gmail.com>
2025-10-16 17:58:17 -0600

    working on algorithm for loads.

--- web/fleet-dispatch-web/src/app/dispatcher/page.tsx
+++ web/fleet-dispatch-web/src/app/dispatcher/page.tsx
@@ -1,12 +1,12 @@
 import DispatchBoard from "../components/DispatchBoard";
-import AIBoard from "../components/AIBoard";
+// import AIBoard from "../components/AIBoard";
 import UnassignedBoardMini from "../components/UnassignedBoardMini";
 import { Metadata } from 'next'
 import UnassignedDrivers from "../components/UnassignedDrivers";
 import UnassignedTrucksBoard from "../components/UnassignedTrucksBoard";
 import UnassignedTrailersBoard from "../components/UnassignedTrailersBoard";
 import CreateLoadCard from "../components/CreateLoadCard";
-// import AlgorithmBoard from "../components/AlgorithmBoard";
+import AlgorithmBoard from "../components/AlgorithmBoard";
 
 export const metadata: Metadata = {
   title: 'Dispatcher Dashboard',
@@ -18,7 +18,7 @@ export default function Dispatcher() {
     <main>
       <DispatchBoard />
       {/* <AIBoard/> */}
-      <AIBoard/>
+      <AlgorithmBoard/>
     <UnassignedBoardMini/>
     <CreateLoadCard/>
       <div className="flex flex-row">
